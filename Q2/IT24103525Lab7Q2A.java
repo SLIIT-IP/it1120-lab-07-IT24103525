799dcdec0d7ba2a3a29234b97511c220c26a4628
IT24103525 <it24103525@my.sliit.lk>
2024-09-21 02:03:50 +0530

    Add IT24103525Lab7Q2A.java to Q2

---
+++ Q2/IT24103525Lab7Q2A.java
@@ -0,0 +1,22 @@
+public class IT24103525Lab7Q2A 
+{
+    public static void main(String[]args) 
+  {
+     
+     int symbol[][] = new int[3][4];
+     int i,j;
+    
+     for(i=0;i<=3;i++)
+      {
+        for(j=0;j<=4;j++)
+        {
+          System.out.print("$ ");
+        }
+
+      System.out.println();
+      
+     }
+
+  }
+
+}
