0bcd3ff4d8b96c2fbb72a77bbfdafb4cce388be9
IT24103525 <it24103525@my.sliit.lk>
2024-09-21 02:04:46 +0530

    Add IT24103525Lab7Q2C.java to Q2

---
+++ Q2/IT24103525Lab7Q2C.java
@@ -0,0 +1,21 @@
+public class IT24103525Lab7Q2C
+{
+    public static void main(String[]args) 
+  {
+     
+     int i,j;
+    
+     for(i=5;i>0;i--)
+      {
+        for(j=1;j<=i;j++)
+        {
+          System.out.print(" "+i);
+        }
+
+      System.out.println();
+      
+     }
+
+  }
+
+}
